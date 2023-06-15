463afa303f91eb051a05a9758dd17ee728fc0175
BertinAm <bertinfonge0@gmail.com>
2023-06-15 17:50:57 +0100

    updates

--- carryamgo/settings.py
+++ carryamgo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-4+v66o@1qc*=vzavafa15#fi35rj+gyy0#1v%ne=q=0%i=yt@l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
