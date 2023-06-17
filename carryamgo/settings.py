05cdede84345e60f7c3f5ea96d7071646398c20f
BertinAm <bertinfonge0@gmail.com>
2023-06-17 01:55:33 +0100

    updates

--- carryamgo/settings.py
+++ carryamgo/settings.py
@@ -151,7 +151,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.permissions.IsAuthenticated',
     ],
 }
